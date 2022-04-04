a2bd30d5002acc75d60167a51e519bc70a92ae84
Meir Klemfner <meirk251@outlook.com>
2022-04-04 18:03:32 +0300

    Update tables' varchars with size 50

--- tables.sql
+++ tables.sql
@@ -1,27 +1,27 @@
 CREATE TABLE Worker
 (
   WorkerId INT NOT NULL,
-  Name VARCHAR(30) NOT NULL,
-  Role VARCHAR(30) NOT NULL,
+  Name VARCHAR(50) NOT NULL,
+  Role VARCHAR(50) NOT NULL,
   SalaryPerHour FLOAT NOT NULL,
   JoinDate DATE NOT NULL,
   LeftDate DATE,
   BirthDate DATE NOT NULL,
-  Email VARCHAR(30) NOT NULL,
+  Email VARCHAR(50) NOT NULL,
   PRIMARY KEY (WorkerId)
 );
 
 CREATE TABLE Wing
 (
   WingId INT NOT NULL,
-  Name VARCHAR(30) NOT NULL,
+  Name VARCHAR(50) NOT NULL,
   PRIMARY KEY (WingId)
 );
 
 CREATE TABLE Department
 (
   DepartmentId INT NOT NULL,
-  Name VARCHAR(30) NOT NULL,
+  Name VARCHAR(50) NOT NULL,
   WingId INT NOT NULL,
   PRIMARY KEY (DepartmentId),
   FOREIGN KEY (WingId) REFERENCES Wing(WingId)
