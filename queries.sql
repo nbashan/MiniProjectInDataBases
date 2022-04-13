ce647910de098a9b00d4d2b0b7dda4f000b872d0
Meir Klemfner <meirk251@outlook.com>
2022-04-13 18:54:50 +0300

    Fix queries.sql

--- queries.sql
+++ queries.sql
@@ -1,24 +1,24 @@
 
-SELECT * FROM Department
+SELECT * FROM Department;
 
-SELECT * FROM Wing
+SELECT * FROM Wing;
 
-SELECT * FROM Worker
+SELECT * FROM Worker;
 
--- All workers that currently working
-SELECT * FROM Worker WHERE LeftDate is NULL
+-- All workers that are currently working
+SELECT * FROM Worker WHERE LeftDate is NULL;
 
 -- Number of workers per department
-SELECT DepartmentId, COUNT(*)
-FROM Worker NATURAL JOIN WorksAt NATURAL JOIN Department
-GROUPBY DepartmentId
+SELECT DepartmentId, COUNT(*) AS Workers
+FROM WorksAt NATURAL JOIN Department
+GROUP BY DepartmentId;
 
 -- Number of workers per wing
 SELECT WingId, COUNT(*)
 FROM Worker NATURAL JOIN WorksAt NATURAL JOIN Department NATURAL JOIN Wing
-GROUPBY WingId
+GROUP BY WingId;
 
 -- All departments without workers
 SELECT * FROM Department d
 WHERE d.DepartmentId NOT IN
-(SELECT DepartmentId FROM WorksAt NATURAL JOIN Department)
+(SELECT DepartmentId FROM WorksAt NATURAL JOIN Department);
