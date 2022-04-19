f8fa7ed36bee9243a67902979cdbf43e0a78f927
Meir Klemfner <meirk251@outlook.com>
2022-04-19 19:50:40 +0300

    fix query: workers with the same birthday

--- queries/workers_with_same_birthday.sql
+++ queries/workers_with_same_birthday.sql
@@ -1,4 +1,4 @@
 -- Workers that have the birthday in the same day
 SELECT w1.WorkerId, w1.Name, w1.BirthDate, w2.WorkerId, w2.WorkerId, w2.Name, w2.BirthDate
 FROM Worker w1 JOIN Worker w2
-WHERE TO_CHAR(w1.BirthDate, 'MM-dd') == TO_CHAR(w2.BirthDate, 'MM-dd')
+ON TO_CHAR(w1.BirthDate, 'MM-dd') = TO_CHAR(w2.BirthDate, 'MM-dd')
