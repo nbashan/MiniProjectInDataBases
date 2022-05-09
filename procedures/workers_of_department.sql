cfd3a4b65a44407ae339464f50fc19eb4d859804
Meir Klemfner <meirk251@outlook.com>
2022-05-09 20:52:43 +0300

    Remove procedure WorkersOfDepartment

--- procedures/workers_of_department.sql
+++
@@ -1,8 +0,0 @@
--- Select workers working at a specific department
-CREATE OR REPLACE WorkersOfDepartment (INT departmentId)
-IS
-BEGIN
-    SELECT *
-    FROM WorksAt NATURAL JOIN Worker
-    WHERE DepartmentId == departmentId
-END
