dc2aad7472cad36e2ef65b41e6da9f1dd2eb74e9
Meir Klemfner <meirk251@outlook.com>
2022-05-09 22:27:15 +0300

    Add DepartmentName to v_WorkingWorkersWithDepartment

--- views/working_workers_with_department.sql
+++ views/working_workers_with_department.sql
@@ -1,7 +1,8 @@
 CREATE VIEW v_WorkingWorkersWithDepartment
 AS
-SELECT DepartmentId, WorkerId, Name, Role, SalaryPerHour, Email, JoinDate, BirthDate
-FROM WorksAt NATURAL JOIN
-(SELECT * FROM Worker WHERE LeftDate IS NULL) t
+SELECT DepartmentId, DepartmentName, WorkerId, Name AS WorkerName, Role, SalaryPerHour, Email, JoinDate, BirthDate
+FROM (SELECT DepartmentId, Name AS DepartmentName, WorkerId
+      FROM WorksAt NATURAL JOIN Department) NATURAL JOIN
+(SELECT * FROM Worker WHERE LeftDate IS NULL)
 ORDER BY DepartmentId;
 
