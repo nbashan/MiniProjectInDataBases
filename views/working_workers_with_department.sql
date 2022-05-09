f4c209faafd25158cae72955dfaf7b0d245362ef
Meir Klemfner <meirk251@outlook.com>
2022-05-09 22:09:12 +0300

    Select specific attributes in v_WorkingWorkersWithDepartment

--- views/working_workers_with_department.sql
+++ views/working_workers_with_department.sql
@@ -1,5 +1,6 @@
 CREATE VIEW v_WorkingWorkersWithDepartment
 AS
-SELECT * FROM WorksAt NATURAL JOIN
+SELECT WorkerId, Name, Role, SalaryPerHour, Email, JoinDate, BirthDate, DepartmentId
+FROM WorksAt NATURAL JOIN
 (SELECT * FROM Worker WHERE LeftDate IS NULL) t;
 
