8c7c111cb88fe586575c7fa251c89cea3f941edb
Meir Klemfner <meirk251@outlook.com>
2022-05-09 22:12:46 +0300

    Set DepartmentId at front and order by it in v_WorkingWorkersWithDepartments

--- views/working_workers_with_department.sql
+++ views/working_workers_with_department.sql
@@ -1,6 +1,7 @@
 CREATE VIEW v_WorkingWorkersWithDepartment
 AS
-SELECT WorkerId, Name, Role, SalaryPerHour, Email, JoinDate, BirthDate, DepartmentId
+SELECT DepartmentId, WorkerId, Name, Role, SalaryPerHour, Email, JoinDate, BirthDate
 FROM WorksAt NATURAL JOIN
-(SELECT * FROM Worker WHERE LeftDate IS NULL) t;
+(SELECT * FROM Worker WHERE LeftDate IS NULL) t
+ORDER BY DepartmentId;
 
