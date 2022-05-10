7bfded398e3ccd17d8bdff33dbaf93150b6166a5
Meir Klemfner <meirk251@outlook.com>
2022-05-10 11:03:25 +0300

    Add comment to query worker_roles.sql

--- queries/worker_roles.sql
+++ queries/worker_roles.sql
@@ -1,3 +1,4 @@
+-- Show all roles with their workers and working workers
 SELECT Role, Count(*) as All_, Sum(IsWorking) as Num
 FROM (SELECT Role, IsWorking(LeftDate) AS IsWorking FROM Worker)
 GROUP BY Role
