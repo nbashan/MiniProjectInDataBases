61304ed9057682ff7759b5809c6c080f774067c4
Meir Klemfner <meirk251@outlook.com>
2022-05-10 10:58:59 +0300

    Change query worker_roles.sql for a better graph
    It shows the number of all working workers and all workers on the same
    row

--- queries/worker_roles.sql
+++ queries/worker_roles.sql
@@ -1,4 +1,4 @@
-SELECT Role, IsWorking, Count(*) as Num
+SELECT Role, Count(*) as All_, Sum(IsWorking) as Num
 FROM (SELECT Role, IsWorking(LeftDate) AS IsWorking FROM Worker)
-GROUP BY Role, IsWorking
-ORDER BY Num;
+GROUP BY Role
+ORDER BY All_;
