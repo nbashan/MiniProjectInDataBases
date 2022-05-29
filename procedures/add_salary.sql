965b951761010ef4717064d92a0e42534c710bb4
Meir Klemfner <meirk251@outlook.com>
2022-05-29 18:24:42 +0300

    fix compilation error of procedure add_salary

--- procedures/add_salary.sql
+++ procedures/add_salary.sql
@@ -1,6 +1,6 @@
-CREATE OR REPLACE PROCEDURE AddSalary(workerId INT, amount FLOAT)
+CREATE OR REPLACE PROCEDURE AddSalary(workerId_ INT, amount FLOAT)
 AS
 BEGIN
     UPDATE Worker SET SalaryPerHour=SalaryPerHour + amount
-    WHERE WorkerId = workerId
-end;
+    WHERE WorkerId = workerId_;
+END;
