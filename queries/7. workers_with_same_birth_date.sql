41b1dad9cd34494ef2a26a31ed81832db582cde9
Meir Klemfner <meirk251@outlook.com>
2022-06-09 01:22:50 +0300

    Add new query 8 workers that came back

--- queries/7. workers_with_same_birth_date.sql
+++ queries/7. workers_with_same_birth_date.sql
@@ -1,6 +1,6 @@
 -- Workers with the same birth date
 SELECT w1.WorkerId, w1.Name, w1.Email, w1.BirthDate, w1.JoinDate, w1.LeftDate
 FROM Worker w1 JOIN Worker w2
-ON TO_CHAR(w1.Birth, 'MM-dd') = TO_CHAR(w2.Birth, 'MM-dd')
+ON TO_CHAR(w1.BirthDate, 'MM-dd') = TO_CHAR(w2.BirthDate, 'MM-dd')
 GROUP BY w1.WorkerId, w1.Name, w1.Email, w1.BirthDate, w1.JoinDate, w1.LeftDate
-ORDER BY TO_CHAR(w1.Birth, 'MM-dd');
+ORDER BY TO_CHAR(w1.BirthDate, 'MM-dd');
