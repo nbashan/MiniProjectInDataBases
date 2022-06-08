547dc9d03e7e07a893c39577eb0fffbbc3e85a0a
Meir Klemfner <meirk251@outlook.com>
2022-06-08 12:42:22 +0300

    Fix query workers_per_department to show working workers

--- queries/workers_per_department.sql
+++ queries/workers_per_department.sql
@@ -1,5 +1,5 @@
 -- Number of working workers per department
 SELECT DepartmentId, Name, COUNT(*) AS Workers
 FROM WorksAt NATURAL JOIN Department
-WHERE WorkerId NOT IN (SELECT WorkerId FROM Worker WHERE LeftDate IS NULL)
+WHERE WorkerId IN (SELECT WorkerId FROM Worker WHERE LeftDate IS NULL)
 GROUP BY DepartmentId, Name;
