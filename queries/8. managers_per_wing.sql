3d76d532cfaefd839776b799fb66a0a34c3a85b2
Meir Klemfner <meirk251@outlook.com>
2022-06-08 14:43:08 +0300

    Fix query 8

--- queries/8. managers_per_wing.sql
+++ queries/8. managers_per_wing.sql
@@ -2,4 +2,4 @@
 SELECT wi.WingId, wi.Name, DepartmentId, d.Name, WorkerId, wo.Name, Email, JoinDate
 FROM (Wing wi INNER JOIN Department d ON d.WingId = wi.WingId)
      NATURAL JOIN WorksAt NATURAL JOIN Worker wo
-WHERE LeftDate IS NULL Role = 'Manager';
+WHERE LeftDate IS NULL AND Role = 'Manager';
