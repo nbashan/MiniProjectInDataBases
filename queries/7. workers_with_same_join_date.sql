fb273f0a277959fff0fc8dc24a3ddf4e5efe0ba0
Meir Klemfner <meirk251@outlook.com>
2022-06-09 00:53:49 +0300

    update query 11 and rename to 7

--- queries/7. workers_with_same_join_date.sql
+++ queries/7. workers_with_same_join_date.sql
@@ -1,4 +1,4 @@
 -- Workers that joined the same day
-SELECT w1.WorkerId, w1.Name, w1.JoinDate, w1.Email
+SELECT w1.WorkerId, w1.Name, w1.Email, w1.JoinDate, w1.LeftDate
 FROM Worker w1 JOIN Worker w2
 ON w1.JoinDate = w2.JoinDate AND EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM w1.JoinDate) >= 5;
