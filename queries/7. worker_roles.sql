a66ef43bd04024f8ac5cc733c5cfa69896c3d903
Meir Klemfner <meirk251@outlook.com>
2022-06-08 13:09:56 +0300

    Update query worker_roles

--- queries/7. worker_roles.sql
+++ queries/7. worker_roles.sql
@@ -1,5 +1,5 @@
 -- Show all roles with their workers and working workers
 SELECT Role, Count(*) AS All_, Sum(IsWorking) AS Current_
-FROM (SELECT Role, IsWorking(LeftDate) AS IsWorking FROM Worker)
+FROM (SELECT Role, LeftDate IS NULL AS IsWorking FROM Worker)
 GROUP BY Role
 ORDER BY All_;
