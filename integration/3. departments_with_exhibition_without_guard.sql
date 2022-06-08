770101d82b0b9c143df8d5d5f96a3e214be725f1
Meir Klemfner <meirk251@outlook.com>
2022-06-08 22:26:26 +0300

    Add integration query 3

---
+++ integration/3. departments_with_exhibition_without_guard.sql
@@ -0,0 +1,8 @@
+-- Finds departments that currently contains an exhibition but without any guards.
+SELECT DepartmentId, d.Name
+FROM Department INNER JOIN Soria.ExhibitionAtDepartment ON DepartmentId = Department_Id
+WHERE DepartmentId NOT IN (
+    SELECT DepartmentId
+    FROM Department NATURAL JOIN WorksAt wa INNER JOIN Worker w ON wa.WorkerId = w.WorkerId
+    WHERE LeftDate IS NULL AND Role = 'Guard';
+);
