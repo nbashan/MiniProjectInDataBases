8aeedffacc9e5c4a271ef31b8138b07f169bed5c
Meir Klemfner <meirk251@outlook.com>
2022-06-09 01:41:10 +0300

    Set query 8 to departments_without_managers

--- queries/8. workers_that_came_back.sql
+++
@@ -1,4 +0,0 @@
--- Workers that worked before, left and came back
-SELECT w1.WorkerId, w1.Name, w1.Email, w1.SalaryPerHour, w1.JoinDate
-FROM Worker w1 JOIN Worker w2 ON w1.BirthDate = w2.BirthDate AND w1.Name = w2.Name
-WHERE w1.LeftDate IS NULL AND w2.LeftDate IS NOT NULL
