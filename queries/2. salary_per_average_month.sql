527218d66a6a3184d3594247e859960132d7dc18
Meir Klemfner <meirk251@outlook.com>
2022-06-08 14:00:43 +0300

    Update query 2 to calculate 20 days

--- queries/2. salary_per_average_month.sql
+++ queries/2. salary_per_average_month.sql
@@ -1,3 +1,3 @@
--- Calculate the salary to pay for workers per an average month (30 days with 8 hours per day)
-SELECT SUM(SalaryPerHour) * 8 * 30 AS Salary
+-- Calculate the salary to pay for workers per an average month (20 days with 8 hours per day)
+SELECT SUM(SalaryPerHour) * 8 * 20 AS Salary
 FROM Worker WHERE LeftDate IS NULL;
