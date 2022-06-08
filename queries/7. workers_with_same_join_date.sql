0528835d81ff1468ecb83e9e06fb82d6bc1c0dfc
Meir Klemfner <meirk251@outlook.com>
2022-06-09 00:55:53 +0300

    Add agregation to query 7

--- queries/7. workers_with_same_join_date.sql
+++ queries/7. workers_with_same_join_date.sql
@@ -1,4 +1,5 @@
 -- Workers that joined the same day
 SELECT w1.WorkerId, w1.Name, w1.Email, w1.JoinDate, w1.LeftDate
 FROM Worker w1 JOIN Worker w2
-ON w1.JoinDate = w2.JoinDate AND EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM w1.JoinDate) >= 5;
+ON w1.JoinDate = w2.JoinDate AND EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM w1.JoinDate) >= 5
+GROUP BY w1.WorkerId, w1.Name, w1.Email. w1.JoinDate, w1.LeftDate;
