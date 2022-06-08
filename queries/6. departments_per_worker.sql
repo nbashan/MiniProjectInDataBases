9486a98027d774cc83ce8f9d8c932a8d403afcbc
Meir Klemfner <meirk251@outlook.com>
2022-06-08 14:06:47 +0300

    Add salary per hour for query 6

--- queries/6. departments_per_worker.sql
+++ queries/6. departments_per_worker.sql
@@ -1,5 +1,5 @@
 -- Number of departments each working worker works on
-SELECT WorkerId, Name, JoinDate, Email, COUNT(*) AS Departments
+SELECT WorkerId, Name, SalaryPerHour, JoinDate, Email, COUNT(*) AS Departments
 FROM WorksAt NATURAL JOIN Worker
 WHERE LeftDate IS NULL
 GROUP BY WorkerId, Name, JoinDate, Email;
