37c8358aef9f7689efd4d385e4cd7f9ceb2439e6
Meir Klemfner <meirk251@outlook.com>
2022-06-08 22:11:15 +0300

    Add integration query 2 departments_without_exhibition

---
+++ integration/2. departments_without_exhibition.sql
@@ -0,0 +1,7 @@
+-- Find all departments that currently don't contains any exhibitions.
+SELECT DepartmentId, Name FROM Department
+WHERE DepartmentId NOT IN (
+    SELECT Department_Id
+    FROM Soria.ExhibitionAtDepartment NATURAL JOIN Soria.Exhibition
+    WHERE StartDate <= CURRENT_DATE AND CURRENT_DATE <= EndDate
+);
