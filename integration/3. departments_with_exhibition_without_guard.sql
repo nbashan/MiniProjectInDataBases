d57c0c9d62f959e2e81edfc7939d3bde417afc72
Meir Klemfner <meirk251@outlook.com>
2022-06-08 22:31:36 +0300

    Fix integration query 3: select current exhibitions

--- integration/3. departments_with_exhibition_without_guard.sql
+++ integration/3. departments_with_exhibition_without_guard.sql
@@ -1,7 +1,9 @@
 -- Finds departments that currently contains an exhibition but without any guards.
 SELECT DepartmentId, Name
-FROM Department INNER JOIN Soria.ExhibitionAtDepartment ON DepartmentId = Department_Id
-WHERE DepartmentId NOT IN (
+FROM Department
+     INNER JOIN Soria.ExhibitionAtDepartment ON DepartmentId = Department_Id
+     NATURAL JOIN Soria.Exhibition
+WHERE StartTime <= CURRENT_DATE AND CURRENT_DATE <= EndTime AND DepartmentId NOT IN (
     SELECT DepartmentId
     FROM Department NATURAL JOIN WorksAt wa INNER JOIN Worker w ON wa.WorkerId = w.WorkerId
     WHERE LeftDate IS NULL AND Role = 'Guard'
